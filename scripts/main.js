64d0a80b473bfa27e8f613206378a0932efb3006
cabonis <cabonis@gmail.com>
2022-03-24 13:25:58 -0400

    Add boilerplate gauges

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,18 @@
 
 import UsMap from './usmap.js'; 
+import GaugeFactory from './gauge.js'; 
 
 let map = new UsMap("hotspots");
+
+let gaugeFactory = new GaugeFactory();
+let gauge1 = gaugeFactory.createGauge("cases");
+let gauge2 = gaugeFactory.createGauge("deaths");
+let gauge3 = gaugeFactory.createGauge("vaccines");
+let gauge4 = gaugeFactory.createGauge("hospitals");
+
+setInterval(() => {
+    gauge1.percent = Math.random();
+    gauge2.percent = Math.random();
+    gauge3.percent = Math.random();
+    gauge4.percent = Math.random();
+}, 2000);
