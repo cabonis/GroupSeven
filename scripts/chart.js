ac77f79e01cf6967ee69f2eff58b8f84cd077298
jchg259 <98977385+jchg259@users.noreply.github.com>
2022-04-09 16:56:29 -0400

    Added LineChart (#25)

--- scripts/chart.js
+++ scripts/chart.js
@@ -71,6 +71,9 @@ export default class Chart extends InfoCard {
 
 class LineChartSvg extends ChartSvg {
 
+    #xScale;
+    #yScale;
+        
     constructor(id) {
         
         const width = 400;
@@ -79,12 +82,41 @@ class LineChartSvg extends ChartSvg {
 
         super(id, width, height, margin);
 
-        this.chart.append("text")
-            .attr("transform", `translate(${width / 4}, ${height / 2})`)
-            .text("Line Chart Placeholder");
+        this.#xScale = d3.scaleBand()
+            .domain([0, 100])
+            .range([ 0, this.width ])
+            .padding(0.2);
+
+        this.xAxis = this.chart.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .attr("class", "linechart axis");
+
+        this.#yScale = d3.scaleLinear()
+            .domain([0, 100])
+            .range([height, 0]);
+
+        this.yAxis = this.chart.append("g")
+            .attr("transform", `translate(0, 0)`)
+            .attr("class", "linechart axis");
     }
 
     update(data) {
+        this.#xScale.domain(data.map(d => d.name));
+        this.xAxis.transition().duration(this.animationDuration).call(d3.axisBottom(this.#xScale));
+    
+        this.#yScale.domain([0, d3.max(data, d => +d.value)]);
+        this.yAxis.transition().duration(this.animationDuration).call(d3.axisLeft(this.#yScale));
+    
+        this.chart.selectAll("line")
+            .data(data)
+            .join("line")
+            .transition()
+            .duration(this.animationDuration)
+                .attr("x", d => this.#xScale(d.name))
+                .attr("y", d => this.#yScale(d.value))
+                .attr("width", this.#xScale.bandwidth())
+                .attr("height", d => this.height - this.#yScale(d.value))
+                .attr("class", "line");
 
     }
 }
